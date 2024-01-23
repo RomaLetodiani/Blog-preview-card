ee3ebb636fc5da8330102cfb738d990d46435f9e
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-23 11:37:10 +0400

    fixed deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tailwind from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/blog-preview-card',
+  base: '/Blog-preview-card',
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
 });
